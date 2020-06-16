e6ed5f3348912360a9f021d547919b870b069e32
Philippe Martin <contact@elol.fr>
2020-06-16 14:22:34 +0200

    refactor

--- pkg/config/chapter.go
+++ pkg/config/chapter.go
@@ -41,44 +41,50 @@ func (o *Chapter) populate(part *Part, toc *TOC, thespec *kubernetes.Spec) error
 	toc.DocumentedDefinitions[o.Key] = []string{o.Name}
 
 	if o.isResource() {
-		suffixes := []string{"Spec", "Status"}
-		for _, suffix := range suffixes {
-			resourceName := o.Name + suffix
-			gvRes := kubernetes.Resource{
-				GVKExtension: kubernetes.GVKExtension{
-					Group:   *o.Group,
-					Version: *o.Version,
-				},
-			}
-			keys := thespec.GVToKey[gvRes.GetGV()]
-			for _, key := range keys {
-				resourceKey := kubernetes.Key(key + "." + resourceName)
-				o.addDefinition(resourceName, resourceKey, part, toc, thespec)
-			}
-		}
-
-		suffixes = []string{"List"}
-		for _, suffix := range suffixes {
-			resourceName := o.Name + suffix
-			key, resource := thespec.GetResource(*o.Group, *o.Version, kubernetes.APIKind(resourceName), true)
-			if resource != nil {
-				newSection := NewSection(resourceName, resource)
-				o.Sections = append(o.Sections, newSection)
-				toc.LinkEnds.Add(key, []string{part.Name, o.Name + "-" + o.Version.String(), newSection.Name})
-				toc.DocumentedDefinitions[key] = []string{resourceName}
-			}
-		}
-
+		o.searchDefinitionsFromResource([]string{"Spec", "Status"}, part, toc, thespec)
+		o.searchResourcesFromResource([]string{"List"}, part, toc, thespec)
 	} else {
+		o.searchDefinitionsFromDefinition([]string{"Status"}, part, toc, thespec)
+	}
+	return nil
+}
 
-		suffixes := []string{"Spec", "Status", "List"}
-		for _, suffix := range suffixes {
-			resourceName := o.Name + suffix
-			resourceKey := kubernetes.Key(o.Key.String() + suffix)
+func (o *Chapter) searchDefinitionsFromResource(suffixes []string, part *Part, toc *TOC, thespec *kubernetes.Spec) {
+	for _, suffix := range suffixes {
+		resourceName := o.Name + suffix
+		gvRes := kubernetes.Resource{
+			GVKExtension: kubernetes.GVKExtension{
+				Group:   *o.Group,
+				Version: *o.Version,
+			},
+		}
+		keys := thespec.GVToKey[gvRes.GetGV()]
+		for _, key := range keys {
+			resourceKey := kubernetes.Key(key + "." + resourceName)
 			o.addDefinition(resourceName, resourceKey, part, toc, thespec)
 		}
 	}
-	return nil
+}
+
+func (o *Chapter) searchResourcesFromResource(suffixes []string, part *Part, toc *TOC, thespec *kubernetes.Spec) {
+	for _, suffix := range suffixes {
+		resourceName := o.Name + suffix
+		key, resource := thespec.GetResource(*o.Group, *o.Version, kubernetes.APIKind(resourceName), true)
+		if resource != nil {
+			newSection := NewSection(resourceName, resource)
+			o.Sections = append(o.Sections, newSection)
+			toc.LinkEnds.Add(key, []string{part.Name, o.Name + "-" + o.Version.String(), newSection.Name})
+			toc.DocumentedDefinitions[key] = []string{resourceName}
+		}
+	}
+}
+
+func (o *Chapter) searchDefinitionsFromDefinition(suffixes []string, part *Part, toc *TOC, thespec *kubernetes.Spec) {
+	for _, suffix := range suffixes {
+		resourceName := o.Name + suffix
+		resourceKey := kubernetes.Key(o.Key.String() + suffix)
+		o.addDefinition(resourceName, resourceKey, part, toc, thespec)
+	}
 }
 
 func (o *Chapter) addDefinition(resourceName string, resourceKey kubernetes.Key, part *Part, toc *TOC, thespec *kubernetes.Spec) {
